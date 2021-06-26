537e69604e8960e79b490a9c2f9c110b9c5cc886
Karl-Johan Westhoff <karl.johan.westhoff@gmail.com>
2021-06-26 10:37:41 -0700

    Schema for Client

--- app/api/models/Client.py
+++ app/api/models/Client.py
@@ -1,11 +1,41 @@
+from sqlalchemy.sql.expression import null
+from sqlalchemy.sql.schema import ForeignKey
 from app.api.db import Base
 from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import validates
+
+import sqlalchemy.orm as orm
+
+
+class Country(Base):
+    __tablename__ = "countries"
+    id = Column(Integer, primary_key=True)
+    name = Column(String(200), nullable=True)
+    name_2letter = Column(String(2), nullable=True)
+    name_3letter = Column(String(3), nullable=True)
+    region = Column(String(15), nullable=True)
 
 
 class Client(Base):
     __tablename__ = 'clients'
     id = Column(Integer, primary_key=True)
-    country = Column(String(50), nullable=False)
+    name = Column(String(50), nullable=False)
+    country = Column(Integer, ForeignKey('countries.id'))
     address = Column(String(50), nullable=False, unique=False)
+
+class Contact(Base):
+    __tablename__ = "contacts"
+    id = Column(Integer, primary_key=True)
+    name = Column(String(50), nullable=False, unique=True)
+    email = name = Column(String(50), nullable=False)
+
+    @validates('email')
+    def validate_email(self, key, email):
+    # make sure email address contains @ character
+    assert '@' in email
+    return email
+
+
     
+
 
--- seeds.py
+++ seeds.py
@@ -1,4 +1,4 @@
-from app.api.models import Client
+from app.api.models.Client import Client, Country
 from app.api.db import Session, Base, engine
 
 #destroy it all..
@@ -6,9 +6,36 @@ Base.metadata.drop_all(engine)
 Base.metadata.create_all(engine)
 
 
-#-------------------------
-#---- Generate random data
-#-------------------------
+
+db = Session()
+
+#----------------------------
+#---- Generate some countries
+#----------------------------
+
+# Get countries
+import json
+with open("app/api/assets/countries.json") as f:
+    countries_dict = json.load(f)
+
+
+#print(countries_dict)
+
+# seed the db
+
+for c in countries_dict:
+    #print(c.keys())
+    db.add(Country(name = c["name"], name_2letter = c["alpha-2"], name_3letter = c["alpha-3"], region = c["region"]))
+    db.commit()
+db.close()
+
+
+
+
+
+#--------------------------------
+#---- Generate random client data
+#--------------------------------
 
 import random
 
@@ -16,21 +43,19 @@ n = 20
 
 prefixes=('Equity','Shale','Dirty','Greedy','Polluting', 'Money')
 postfixes=('Energy','Power','Oil','GAS','Benzin','Petroleum','Fuel','JetFuel')
+
+
+
 names = []
 for x in range(n):
     fullname = random.choice(prefixes)+" "+random.choice(postfixes)
-    names.append(fullname)
-    
-print(names)
+    country = random.randint(1,len(countries_dict))
+    address = "NA Temp" 
+    db.add_all(
+        [Client(name=fullname,country = country, address = address)]
+    )
+    db.commit()
+
+db.close()       
 
 
-
-db = Session()
-
-
-
-
-# insert  clients
-#db.add_all(
-#    Client()
-#)
